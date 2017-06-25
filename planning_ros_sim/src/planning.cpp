c8ee76ed72caff84b10cb13ab927f1cc9267eb8e
Rasmus Munter <rasmusmunter@gmail.com>
2017-06-25 15:29:35 +0200

    More bug fixes

--- planning_ros_sim/src/planning.cpp
+++ planning_ros_sim/src/planning.cpp
@@ -5,12 +5,9 @@
 #include "../../../devel/include/planning_ros_sim/groundRobot.h"
 #include "../../../devel/include/planning_ros_sim/droneCmd.h"
 #include <actionlib/client/simple_action_client.h>
-
 #include <stdio.h>
 #include "AI/AI.h"
 #include "AI/structs.h"
-
-
 
 using namespace std;
 
@@ -35,24 +32,22 @@ AI* ai = new AI();
 
 void groundRobot_chatterCallback(const planning_ros_sim::groundRobotList &msg)
 {
-  ai->robot_update(msg);
+  //Probably need a msg to observation_t function
+  ai->update(msg);
 }
 
 void drone_chatterCallback(geometry_msgs::Pose2D msg)
 {
-	
-  ai->drone_update(msg)
-
-};
+  //Probably need a msg to observation_t function
+  ai->update(msg)
+}
 
 planning_ros_sim::droneCmd drone_action(planning_ros_sim::droneCmd drone_action)
 {
 	drone_pos.x = 10;
 	drone_pos.y = 10;
 	drone_pos.z = 1;
-	sim_Command command;
-  command.type = sim_CommandType_LandInFrontOf;
-	drone_pos.cmd = command.type;
+	action_t command;  
   return drone_pos;
 }
 
@@ -83,16 +78,17 @@ int main(int argc, char **argv)
       //If we've finished our stack get a new one!
       if(chosen_action_stack.empty()){
         current_action_stack = ai->getBestGeneralActionStack();
+        target_id = current_action_stack.top().target;
       }
  
       //If we are waiting on the ground robot(ie the robot isn't
       //nearby our landing location) we might aswell update our
       //where_to_act on our current observations.
-      if(!nearby(current_action_stack.where_to_act, target)){
+      if(!nearby(current_action.where_to_act, target)){
         current_action_stack = updated_action_stack;
-        current_action = current_action_stack.pop(); 
       }
 
+      current_action = current_action_stack.pop(); 
       drone_action = drone_action(current_action);
       command_pub.publish(drone_action);
 
@@ -102,7 +98,7 @@ int main(int argc, char **argv)
 
     //If the action we are currently doing is significantly different
     //from the best possible action, abort.
-    if(similarity(current_action ,updated_target_actions.top())  > SIMILARITY_THRESHOLD){
+    if(similarity(current_action ,updated_action_stack.top())  > SIMILARITY_THRESHOLD){
       current_action_stack = ai->getBestGeneralActionStack();
       cancle_actionlib_action();
     }
