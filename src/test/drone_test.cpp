d768c7ba315480c9cf8f7bc047c8f04fc3259bd7
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-10-05 16:03:17 +0200

    Finish tests for distance from drone to point

--- src/test/drone_test.cpp
+++ src/test/drone_test.cpp
@@ -16,13 +16,33 @@ public:
 
 TEST (DroneTest, EmptyConstructorTest) {
 	Drone* drone = new Drone();
-	point_t pos = drone->getPosition();
-	EXPECT_EQ(pos.x, 0);
-	EXPECT_EQ(pos.y, 0);
+	point_t position = drone->getPosition();
+	EXPECT_EQ(position.x, 0);
+	EXPECT_EQ(position.y, 0);
 }
 
 TEST (DroneTest, DistanceToPointTest) {
-	// Lag en test her...
+	Drone* drone = new Drone();
+	point_t point = point_Zero;
+	point.x = 3;
+	point.y = 4;
+	EXPECT_EQ(drone->getDistanceToPoint(point), 5);
+
+	point.x = -3;
+	point.y = 4;
+	EXPECT_EQ(drone->getDistanceToPoint(point), 5);
+
+	point.x = 0;
+	point.y = 0;
+	EXPECT_EQ(drone->getDistanceToPoint(point), 0);
+
+	point.x = 5;
+	point.y = -5;
+	EXPECT_NEAR(drone->getDistanceToPoint(point), 7.07f, 0.01f);
+
+	point.x = 123456789;
+	point.y = 987654321;
+	EXPECT_NEAR(drone->getDistanceToPoint(point), 995340462.62581f, 0.0001f);
 }
 
 int main(int argc, char **argv){
