3ccc6fed4dda8289f71677914456f195355f1ffa
Alex <alexada@stud.ntnu.no>
2017-10-11 18:29:07 +0200

    add doxygen to drone

--- src/AI/Drone.h
+++ src/AI/Drone.h
@@ -1,3 +1,12 @@
+
+/**
+@class Drone
+@brief Drone class
+This calss holds the information for the drone as updated by the simulator and the AI.
+It also includes functions for determining the drones relation to other parts of the world (e.g. points and ground robots).
+*/
+
+
 #include <cmath>
 #include <iostream>
 #include "structs.h"
@@ -19,17 +28,32 @@ private:
 
 public:
 	Drone();
-	//get
+	///return position of drone on the field
 	point_t getPosition();
+
+	///return the current state of the drone (e.g. choosing target/action, landing on top or in front, etc.)
 	drone_State_t getState();
 
-	//update
+	///update drone based on the new observation from the sim
 	bool update(observation_t observation);
 
 
 	//actions
 	void wait(float time);
+
+	/**
+	@brief Calculates and returns the straight line distance between the drones current position and the input point
+	@param point - point struct to calculate distance to
+	@return float representing distance in meters between the drone and the given point
+	*/
 	float getDistanceToPoint(point_t point);
+	
 	// float getTravelTimeToPoint(point_t point);
+	
+	/**
+	@brief Finds the earliest point where the drone and the robot can intercept
+	@param robot - a robot object refering to the given robot
+	@return a point struct defining the earliest possible intecpet point
+	*/
 	point_t getInterceptPoint(Robot robot);
 };
