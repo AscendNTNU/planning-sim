21a953cab54c27260e9c4e3c3e7b58b29c01f4a4
Unknown <rasmusmunter@gmail.com>
2017-10-17 19:38:59 +0200

    Added docstrings to relevant functions

--- src/planning.cpp
+++ src/planning.cpp
@@ -1,3 +1,4 @@
+//ROS includes
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Float32.h"
@@ -7,34 +8,38 @@
 #include "planning_ros_sim/groundRobot.h"
 #include "planning_ros_sim/droneCmd.h"
 #include <actionlib/client/simple_action_client.h>
+
+//Code specific includes
 #include <stdio.h>
 #include "AI/AI.h"
 #include "AI/structs.h"
-#include <assert.h>
+#include "planning_callbacks.h"
 
 const float SIMILARITY_THRESHOLD = 1;
+
+float elapsed_time = 0;
+bool action_done = true;
 
 planning_ros_sim::groundRobotList GroundRobots;
 geometry_msgs::Pose2D Drone;
 
-float elapsed_time = 0;
-
-bool action_done = true;
-
 AI ai = AI();
 World world = World(0);
 
+
+//Callback functions
 void time_chatterCallback(std_msgs::Float32 msg){
     elapsed_time = (float)msg.data;
 }
+
 
 void groundRobot_chatterCallback(const planning_ros_sim::groundRobotList &msg)
 {
     observation_t robotObs;
     for(int i = 0; i < 10; i++) {
-    	  robotObs.robot_x[i] = msg.groundRobot[i].x;
-    	  robotObs.robot_y[i] = msg.groundRobot[i].y;
-    	  robotObs.robot_q[i] = msg.groundRobot[i].theta;
+          robotObs.robot_x[i] = msg.groundRobot[i].x;
+          robotObs.robot_y[i] = msg.groundRobot[i].y;
+          robotObs.robot_q[i] = msg.groundRobot[i].theta;
     }
     ai.updateRobot(robotObs, elapsed_time);
 }
@@ -51,6 +56,8 @@ void command_done_chatterCallback(std_msgs::Bool msg){
     action_done = (bool)msg.data;
 }
 
+//helper functions
+//Converts AI's drone command to the correct ROS message type
 planning_ros_sim::droneCmd to_ROS_Command(action_t action)
 {
     planning_ros_sim::droneCmd command;
@@ -64,6 +71,7 @@ planning_ros_sim::droneCmd to_ROS_Command(action_t action)
     return command;
 }
 
+//Checks if a point is near another point and returns true if it is below the SIMILARITY_THRESHOLD
 bool is_nearby(point_t current_Where_To_Act, point_t target) {
 	  double x1 = current_Where_To_Act.x;
 	  double y1 = current_Where_To_Act.y;
@@ -74,6 +82,7 @@ bool is_nearby(point_t current_Where_To_Act, point_t target) {
 	  return dist < SIMILARITY_THRESHOLD;
 }
 
+//Checks similarity between actions, currently just checks if they are identical or not
 float similarity(action_t action1 ,action_t action2){
     if(is_nearby(action1.where_To_Act, action2.where_To_Act)){
         return 1;
@@ -81,8 +90,10 @@ float similarity(action_t action1 ,action_t action2){
     return 0;
 }
 
+//main
 int main(int argc, char **argv)
 {
+    //Ros initialization
     ros::init(argc, argv, "planning");
     ros::NodeHandle ground_robot_node;
     ros::NodeHandle drone_node;
@@ -99,44 +110,47 @@ int main(int argc, char **argv)
 
     planning_ros_sim::droneCmd drone_action;
     
+    //Initialization of variables
     int target_id = 0;
     Robot target = ai.state.getRobot(1);
     action_t current_action;
     std::stack<action_t> current_action_stack;
-
     world.startTimer();
 
+    //Wait for real data before starting AI algorithm
     while (ros::ok() and ai.state.getRobot(0).getPosition().x == 0){
         ros::Duration(0.2).sleep();
         ros::spinOnce();
     }
 
+    //AI algorithm
     while (ros::ok()){
         ros::Duration(0.4).sleep();
         ros::spinOnce();
+
         if(action_done){
             target = ai.state.getRobot(target_id);
-            //If we've finished our stack get a new one!
+            
+            //If the stack of actions is empty get a new one!
             if(current_action_stack.empty()){
                 current_action_stack = ai.getBestGeneralActionStack();
                 target_id = current_action_stack.top().target;
                 target = ai.state.getRobot(target_id);
-                std::cout << "1" << std::endl;
             }
+
             //If we are waiting on the ground robot(ie the robot isn't
             //nearby our landing location) we might aswell update our
             //where_to_act on our current observations.
             else if(current_action_stack.top().type != search && !is_nearby(current_action_stack.top().where_To_Act, target.getPosition())){
                 current_action_stack.push(ai.getBestActionStack(target).top());
-                std::cout << "2" << std::endl;
 
             }
 
+            //Send next action to drone
             current_action = current_action_stack.top();
             drone_action = to_ROS_Command(current_action);
             command_pub.publish(drone_action);
             current_action_stack.pop();
-            std::cout << "3" << std::endl;
         }
     }
     return 0;
