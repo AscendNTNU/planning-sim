05e2e36ee82bc8f777d20ba8ad7e18ae4f6ca433
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-11-04 16:30:35 +0100

    Add a visibility variable to robots and a patrol sequence if none is seen

--- src/AI/AI.cpp
+++ src/AI/AI.cpp
@@ -1,63 +1,113 @@
 #include "AI.h"
+#include "Robot.h"
 #include <typeinfo>
-AI::AI(){
+AI::AI() {
     this->state = State();
 }
 
-std::stack<action_t> AI::getBestGeneralActionStack(int num_Robots){
+std::stack<action_t> AI::getBestGeneralActionStack(int num_Robots) {
     Robot target = chooseTarget(10);
     return getBestActionStack(target);
 }
 
-std::stack<action_t> AI::getBestActionStack(Robot target){
-    
+std::stack<action_t> AI::getBestActionStack(Robot target) {
     std::stack<action_t> action_Stack;
 
     action_t best_Action = chooseAction(target);
-
-    action_t search_Action = best_Action;
-    search_Action.type = search;
-
     action_Stack.push(best_Action);
-    action_Stack.push(search_Action);
+
+    // If search is not already spesified, add a search (goto) action to complete the next action
+    if (best_Action.type != search) {
+        action_t search_Action = best_Action;
+        search_Action.type = search;
+        action_Stack.push(search_Action);
+    }
 
     return action_Stack;
 }
 
-Robot AI::chooseTarget(int num_Robots){
+Robot AI::chooseTarget(int num_Robots) {
     Robot robot;
-    Robot target;
     float best_reward = -1000000;
-    for(int i = 0; i < num_Robots; i++){
+
+    // Return an invalid robot if none was assigned
+    Robot target = Robot(-1);
+
+    for (int i = 0; i < num_Robots; i++) {
         robot = this->state.robots[i];
-        if(robot.getIndex() != -1){
-            if(robot.current_Plank.getReward() > best_reward && !robot.current_Plank.willExitGreen()){
+
+        if (robot.getIndex() != -1 && robot.getVisibility()) {
+
+            if (robot.current_Plank.getReward() > best_reward && !robot.current_Plank.willExitGreen()) {
                 best_reward = robot.current_Plank.getReward();
                 target = robot;
             }
         }
     }
+
     return target;
 }
 
-action_t AI::chooseAction(Robot target){
+action_t AI::chooseAction(Robot target) {
 
     // // Temporary max rewarded action
     action_t best_Action = action_Empty;
-    
+
     // best_Action.where_To_Act.travel_Time = interception.travel_Time;
-    
-    action_t step_Action;
 
-    for(int i = 1; i < target.current_Plank.getNumPlankPoints()-1; i++) {
-      std::cout << "Plank point " << i << ": " << target.current_Plank.getPoint(i).point.x << ", " << target.current_Plank.getPoint(i).point.y << std::endl;
+    // Check if we have a visible target
+    if (target.getIndex() != -1) {
+        action_t step_Action;
 
-      step_Action = getBestActionAtPosition(target.getOrientation(), target.current_Plank.getPoint(i));
-      if (step_Action.reward > best_Action.reward) {
-          best_Action = step_Action;
-      }
+        for (int i = 1; i < target.current_Plank.getNumPlankPoints() - 1; i++) {
+            std::cout << "Plank point " << i << ": " << target.current_Plank.getPoint(i).point.x << ", " << target.current_Plank.getPoint(i).point.y << std::endl;
+
+            step_Action = getBestActionAtPosition(target.getOrientation(), target.current_Plank.getPoint(i));
+
+            if (step_Action.reward > best_Action.reward) {
+                best_Action = step_Action;
+            }
+        }
+
+        best_Action.target = target.getIndex();
     }
-    best_Action.target = target.getIndex();
+    // If no target is visible, we do a patrol round
+    else {
+        action_t search_Action;
+
+        point_t nextSearchPoint = point_Zero;
+
+        point_t pos_ = this->state.drone.getPosition();
+        float x_ = pos_.x;
+        float y_ = pos_.y;
+        float trackWidth_ = 20;
+        float trackHeight_ = 20;
+        float trackCenterX_ = trackWidth_ / 2;
+        float trackCenterY_ = trackHeight_ / 2;
+        float padding_ = 3;
+
+        // The drone flies in a square path in a clockwise order
+        if (x_ > trackCenterX_ && y_ > trackCenterY_) {
+            nextSearchPoint.x = trackWidth_ - padding_;
+            nextSearchPoint.y = padding_;
+        } else if (x_ > trackCenterX_ && y_ < trackCenterY_) {
+            nextSearchPoint.x = padding_;
+            nextSearchPoint.y = padding_;
+        } else if (x_ <= trackCenterX_ && y_ <= trackCenterY_) {
+            nextSearchPoint.x = padding_;
+            nextSearchPoint.y = trackHeight_ - padding_;
+        } else if (x_ < trackCenterX_ && y_ > trackCenterY_) {
+            nextSearchPoint.x = trackWidth_ - padding_;
+            nextSearchPoint.y = trackHeight_ - padding_;
+        }
+
+        search_Action.type = search;
+        search_Action.where_To_Act = nextSearchPoint;
+        search_Action.target = 0;
+
+        best_Action = search_Action;
+    }
+
     return best_Action;
 }
 
@@ -75,15 +125,16 @@ action_t AI::getBestActionAtPosition(float target_orientation, plank_point_t pos
     return actionWithMaxReward(plank_On_Top.getReward(), plank_In_Front.getReward(), action);
 }
 
-action_t AI::actionWithMaxReward(float reward_On_Top, float reward_In_Front, action_t action){
+action_t AI::actionWithMaxReward(float reward_On_Top, float reward_In_Front, action_t action) {
 
-    if(reward_On_Top > reward_In_Front){
+    if (reward_On_Top > reward_In_Front) {
         action.type = land_On_Top_Of;
         action.reward = reward_On_Top;
-    } else{
+    } else {
         action.type = land_In_Front_Of;
         action.reward = reward_In_Front;
     }
+
     return action;
 }
 
@@ -91,10 +142,10 @@ bool AI::update(observation_t observation,float elapsed_time) {
     return this->state.updateState(observation, elapsed_time);
 }
 
-bool AI::updateRobot(observation_t observation,float elapsed_time){
+bool AI::updateRobot(observation_t observation,float elapsed_time) {
     return this->state.updateRobotState(observation, elapsed_time);
 }
 
-bool AI::updateDrone(observation_t observation,float elapsed_time){
+bool AI::updateDrone(observation_t observation,float elapsed_time) {
     return this->state.updateDroneState(observation, elapsed_time);
 }
