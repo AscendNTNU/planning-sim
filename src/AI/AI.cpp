955949f9b4d4a2fb88bfd8f9a0cc4fbd30ab5879
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-11-05 18:19:47 +0100

    Change postfixes and apply snake_case to variables

--- src/AI/AI.cpp
+++ src/AI/AI.cpp
@@ -6,123 +6,123 @@ AI::AI() {
 }
 
 std::stack<action_t> AI::getBestGeneralActionStack(int num_Robots) {
-    Robot target_ = chooseTarget(10);
-    return getBestActionStack(target_);
+    Robot target = chooseTarget(10);
+    return getBestActionStack(target);
 }
 
 std::stack<action_t> AI::getBestActionStack(Robot target) {
-    std::stack<action_t> action_Stack_;
+    std::stack<action_t> action_Stack;
 
-    action_t best_Action_ = chooseAction(target);
-    action_Stack_.push(best_Action_);
+    action_t best_Action = chooseAction(target);
+    action_Stack.push(best_Action);
 
     // If search is not already spesified, add a search (goto) action to complete the next action
-    if (best_Action_.type != search) {
-        action_t search_Action_ = best_Action_;
-        search_Action_.type = search;
-        action_Stack_.push(search_Action_);
+    if (best_Action.type != search) {
+        action_t search_Action = best_Action;
+        search_Action.type = search;
+        action_Stack.push(search_Action);
     }
 
-    return action_Stack_;
+    return action_Stack;
 }
 
 Robot AI::chooseTarget(int num_Robots) {
-    Robot robot_;
-    float best_reward_ = -1000000;
+    Robot robot;
+    float best_reward = -1000000;
 
     // Return an invalid robot if none was assigned
-    Robot target_ = Robot(-1);
+    Robot target = Robot(-1);
 
     for (int i = 0; i < num_Robots; i++) {
-        robot_ = this->state.robots[i];
+        robot = this->state.robots[i];
 
-        if (robot_.getIndex() != -1 && robot_.getVisibility()) {
+        if (robot.getIndex() != -1 && robot.getVisibility()) {
 
-            if (robot_.current_Plank.getReward() > best_reward_ && !robot_.current_Plank.willExitGreen()) {
-                best_reward_ = robot_.current_Plank.getReward();
-                target_ = robot_;
+            if (robot.current_Plank.getReward() > best_reward && !robot.current_Plank.willExitGreen()) {
+                best_reward = robot.current_Plank.getReward();
+                target = robot;
             }
         }
     }
 
-    return target_;
+    return target;
 }
 
 action_t AI::chooseAction(Robot target) {
 
     // // Temporary max rewarded action
-    action_t best_Action_ = action_Empty;
+    action_t best_Action = action_Empty;
 
-    // best_Action_.where_To_Act.travel_Time = interception.travel_Time;
+    // best_Action.where_To_Act.travel_Time = interception.travel_Time;
 
     // Check if we have a visible target
     if (target.getIndex() != -1) {
-        action_t step_Action_;
+        action_t step_Action;
 
         for (int i = 1; i < target.current_Plank.getNumPlankPoints() - 1; i++) {
             std::cout << "Plank point " << i << ": " << target.current_Plank.getPoint(i).point.x << ", " << target.current_Plank.getPoint(i).point.y << std::endl;
 
-            step_Action_ = getBestActionAtPosition(target.getOrientation(), target.current_Plank.getPoint(i));
+            step_Action = getBestActionAtPosition(target.getOrientation(), target.current_Plank.getPoint(i));
 
-            if (step_Action_.reward > best_Action_.reward) {
-                best_Action_ = step_Action_;
+            if (step_Action.reward > best_Action.reward) {
+                best_Action = step_Action;
             }
         }
 
-        best_Action_.target = target.getIndex();
+        best_Action.target = target.getIndex();
     }
     // If no target is visible, we do a patrol round
     else {
-        action_t search_Action_ = action_Empty;
+        action_t search_Action = action_Empty;
 
-        point_t nextSearchPoint_ = point_Zero;
+        point_t next_search_point = point_Zero;
 
-        point_t pos_ = this->state.drone.getPosition();
-        float x_ = pos_.x;
-        float y_ = pos_.y;
-        float trackWidth_ = 20;
-        float trackHeight_ = 20;
-        float trackCenterX_ = trackWidth_ / 2;
-        float trackCenterY_ = trackHeight_ / 2;
-        float padding_ = 3;
+        point_t pos = this->state.drone.getPosition();
+        float x = pos.x;
+        float y = pos.y;
+        float track_width = 20;
+        float track_height = 20;
+        float track_center_x = track_width / 2;
+        float track_center_y = track_height / 2;
+        float padding = 3;
 
         // The drone flies in a square path in a clockwise order
-        if (x_ > trackCenterX_ && y_ > trackCenterY_) {
-            nextSearchPoint_.x = trackWidth_ - padding_;
-            nextSearchPoint_.y = padding_;
-        } else if (x_ > trackCenterX_ && y_ < trackCenterY_) {
-            nextSearchPoint_.x = padding_;
-            nextSearchPoint_.y = padding_;
-        } else if (x_ <= trackCenterX_ && y_ <= trackCenterY_) {
-            nextSearchPoint_.x = padding_;
-            nextSearchPoint_.y = trackHeight_ - padding_;
-        } else if (x_ < trackCenterX_ && y_ > trackCenterY_) {
-            nextSearchPoint_.x = trackWidth_ - padding_;
-            nextSearchPoint_.y = trackHeight_ - padding_;
+        if (x > track_center_x && y > track_center_y) {
+            next_search_point.x = track_width - padding;
+            next_search_point.y = padding;
+        } else if (x > track_center_x && y < track_center_y) {
+            next_search_point.x = padding;
+            next_search_point.y = padding;
+        } else if (x <= track_center_x && y <= track_center_y) {
+            next_search_point.x = padding;
+            next_search_point.y = track_height - padding;
+        } else if (x < track_center_x && y > track_center_y) {
+            next_search_point.x = track_width - padding;
+            next_search_point.y = track_height - padding;
         }
 
-        search_Action_.type = search;
-        search_Action_.where_To_Act = nextSearchPoint_;
-        search_Action_.target = 0;
+        search_Action.type = search;
+        search_Action.where_To_Act = next_search_point;
+        search_Action.target = 0;
 
-        best_Action_ = search_Action_;
+        best_Action = search_Action;
     }
 
-    return best_Action_;
+    return best_Action;
 }
 
 action_t AI::getBestActionAtPosition(float target_orientation, plank_point_t position) {
-    int num_Iterations_ = 5; // Number of iterations when summing along a plank
-    action_t action_;
-    action_.where_To_Act = position.point;
-    // float time_After_Turn_Start_ = fmod(this->state.getTimeStamp() + position.travel_Time + time_after_interception, 20);
+    int num_Iterations = 5; // Number of iterations when summing along a plank
+    action_t action;
+    action.where_To_Act = position.point;
+    // float time_After_Turn_Start = fmod(this->state.getTimeStamp() + position.travel_Time + time_after_interception, 20);
 
-    Plank plank_On_Top_ = Plank(position.point, fmod(target_orientation + (MATH_PI/4), 2*MATH_PI), 
+    Plank plank_On_Top = Plank(position.point, fmod(target_orientation + (MATH_PI/4), 2*MATH_PI), 
                                     position.time_since_start_turn);
-    Plank plank_In_Front_ = Plank(position.point, fmod(target_orientation + MATH_PI, 2*MATH_PI), 
+    Plank plank_In_Front = Plank(position.point, fmod(target_orientation + MATH_PI, 2*MATH_PI), 
                                     position.time_since_start_turn);
 
-    return actionWithMaxReward(plank_On_Top_.getReward(), plank_In_Front_.getReward(), action_);
+    return actionWithMaxReward(plank_On_Top.getReward(), plank_In_Front.getReward(), action);
 }
 
 action_t AI::actionWithMaxReward(float reward_On_Top, float reward_In_Front, action_t action) {
