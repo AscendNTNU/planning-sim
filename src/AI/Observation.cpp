ac27cea73cfda9882944b5b0de474516c8668d9e
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-01-12 12:19:05 +0100

    Add new lines at end of file and convert it to space indention

--- src/AI/Observation.cpp
+++ src/AI/Observation.cpp
@@ -1,73 +1,73 @@
 #include "Observation.h"
 
 Observation::Observation(){
-	this->drone = Drone();
-	
-	for(int i = 0; i<10; i++){
-		this->robots[i] = Robot();
-	}
-	
-	for(int i = 0; i<4; i++){
-		this->obstacles[i] = Robot();
-	}
-	this->time_Stamp = 0;
+    this->drone = Drone();
+    
+    for(int i = 0; i<10; i++){
+        this->robots[i] = Robot();
+    }
+    
+    for(int i = 0; i<4; i++){
+        this->obstacles[i] = Robot();
+    }
+    this->time_Stamp = 0;
 }
 
 Drone Observation::getDrone(){
-	return this->drone;
+    return this->drone;
 }
 
 Robot Observation::getRobot(int index){
-	if(index < 0 || index > 10){
-		return Robot(-1);
-	}
-	return this->robots[index];
+    if(index < 0 || index > 10){
+        return Robot(-1);
+    }
+    return this->robots[index];
 }
 
 std::array<Robot,10> Observation::getRobots(){
-	return this->robots;
+    return this->robots;
 }
 
 
 Robot Observation::getObstacle(int index){
-	if(index < 0 || index > 4){
-		return  Robot(-1);
-	}
-	return this->obstacles[index];
+    if(index < 0 || index > 4){
+        return  Robot(-1);
+    }
+    return this->obstacles[index];
 }
 
 std::array<Robot,4> Observation::getObstacles(){
-	return this->obstacles;
+    return this->obstacles;
 }
 
 float Observation::getTimeStamp(){
-	return this->time_Stamp;
+    return this->time_Stamp;
 }
 
 
 bool Observation::update(observation_t observation, float elapsed_time){
-	bool drone_Updated = updateDrone(observation, elapsed_time);
-	bool robot_Updated = updateRobot(observation, elapsed_time);
-	return (drone_Updated and robot_Updated);
+    bool drone_Updated = updateDrone(observation, elapsed_time);
+    bool robot_Updated = updateRobot(observation, elapsed_time);
+    return (drone_Updated and robot_Updated);
 }
 
 bool Observation::updateDrone(observation_t observation, float elapsed_time){
 
-	this->time_Stamp = elapsed_time;
-	this->drone.update(observation);
-	return true;
+    this->time_Stamp = elapsed_time;
+    this->drone.update(observation);
+    return true;
 }
 
 bool Observation::updateRobot(observation_t observation, float elapsed_time){
-	point_t position = point_Zero;
-	this->time_Stamp = elapsed_time;
-	for(int i = 0; i < 10; i++){ // should loop through lenght of observed robots not 10.
-		position = (point_t){.x = observation.robot_x[i], .y = observation.robot_y[i]};
-		this->robots[i].update(i, position, observation.robot_q[i], this->time_Stamp);
-	}
-	for(int i = 0; i < 4; i++){
-		position = (point_t){.x = 0, .y = 0};
-		this->obstacles[i].update(i, position, 0, this->time_Stamp);
-	}
-	return true;
+    point_t position = point_Zero;
+    this->time_Stamp = elapsed_time;
+    for(int i = 0; i < 10; i++){ // should loop through lenght of observed robots not 10.
+        position = (point_t){.x = observation.robot_x[i], .y = observation.robot_y[i]};
+        this->robots[i].update(i, position, observation.robot_q[i], this->time_Stamp);
+    }
+    for(int i = 0; i < 4; i++){
+        position = (point_t){.x = 0, .y = 0};
+        this->obstacles[i].update(i, position, 0, this->time_Stamp);
+    }
+    return true;
 }
--- src/AI/Observation.h
+++ src/AI/Observation.h
@@ -5,26 +5,26 @@
 
 class Observation{
 private:
-	float time_Stamp;
-	std::array<Robot,10> robots;
-	std::array<Robot,4> obstacles;
-	Drone drone;
+    float time_Stamp;
+    std::array<Robot,10> robots;
+    std::array<Robot,4> obstacles;
+    Drone drone;
 
 public:
 
-	Observation();
+    Observation();
 
-	Drone getDrone();
+    Drone getDrone();
 
-	Robot getRobot(int index);
-	std::array<Robot,10> getRobots();
+    Robot getRobot(int index);
+    std::array<Robot,10> getRobots();
 
-	Robot getObstacle(int index);
-	std::array<Robot,4> getObstacles();
+    Robot getObstacle(int index);
+    std::array<Robot,4> getObstacles();
 
-	float getTimeStamp();
+    float getTimeStamp();
 
-	bool update(observation_t observation, float elapsed_time);
-	bool updateDrone(observation_t observation, float elapsed_time);
-	bool updateRobot(observation_t observation, float elapsed_time);
+    bool update(observation_t observation, float elapsed_time);
+    bool updateDrone(observation_t observation, float elapsed_time);
+    bool updateRobot(observation_t observation, float elapsed_time);
 };
