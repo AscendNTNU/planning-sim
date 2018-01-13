1baa4dd105bb2a7df81c7c8f5169a7cd854da32e
Unknown <nadiawangberg98@gmail.com>
2018-01-13 14:27:38 +0100

    Create getActionStack() in TreeSearch

--- src/AI/TreeSearch/TreeSearch.cpp
+++ src/AI/TreeSearch/TreeSearch.cpp
@@ -1,20 +1,22 @@
 #include "TreeSearch.h"
+#include "Node.h"
 #include <iostream>
 #include <list>
+#include <stack>
 
 TreeSearch::TreeSearch(Observation state) {
     this->state_ = state;
 
-    node_t root;
+    Node root;
     root.state = state;
     root.from_action = command_Empty;
     root.reward = 0;
     root.parent = NULL;
     root.root = true;
 
-    std::list<node_t> children;
+    std::list<Node> children;
     for (int i = 0; i < Num_Targets; i++) {
-        node_t child;
+        Node child;
         child.parent = &root;
         root.children.push_front(child);
     }
@@ -22,22 +24,31 @@ TreeSearch::TreeSearch(Observation state) {
     this->root_ = &root;
 }
 
-void TreeSearch::DFS(node_t* node_p) {
+void TreeSearch::DFS(Node* node_p) {
 
     if(node_p->reward > this->best_node_p_->reward){
         this->best_node_p_ = node_p;
     }
 
 
-    std::list<node_t*> children = node_p->children;
-    std::list<node_t*>::iterator it;
+    std::list<Node*> children = node_p->children;
+    std::list<Node*>::iterator it;
 
     for (it = node_p->children.begin(); it != node_p->children.end(); it++) {
-        node_t* child_p = *it;
+        Node* child_p = *it;
         this->DFS(child_p);
     }
 }
 
-std::list<node_t> TreeSearch::getChildren(node_t node) {
+std::list<Node> TreeSearch::getChildren(Node node) { // might be unnecessary
     return node.children;
+}
+
+ std::stack<action_t> TreeSearch::getActionStack(Node* node_p, std::stack<action_t>& action_stack) {
+    action_stack.push(node_p->action);
+
+    if (node_p->parent != NULL) {
+        this->getActionStack(node_p->parent, action_stack);
+    }
+    return action_stack;
 }
