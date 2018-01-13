acdd5d6e43da94ca42ef38f5fa345bad5b018534
Unknown <nadiawangberg98@gmail.com>
2018-01-13 13:02:46 +0100

    Create node class

---
+++ src/AI/TreeSearch/Node.h
@@ -0,0 +1,16 @@
+#include "AI.h"
+#include "structs.h"
+#include "Observation.h"
+#include <list>
+
+class Node {
+    private:
+        Node* parent_p;
+        std::list<Node*> children;
+        Observation state;
+        action_t action; // should be renamed action from?
+        float reward;
+        float time;
+    public:
+        Node(Node* parent_p, Observation state, action_t action);
+};
