fc96ff0790ca2b9120059c480b7b765bc1023526
Adrian Tofting <adrianto@stud.ntnu.no>
2018-02-08 16:10:27 +0100

    Add no input state, work in progress

---
+++ src/AI/state_machine/no_input_state.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "state_interface.h"
+
+///Runs preflight checks and transition to idle when ready
+class NoInputState : public StateInterface {
+public:
+    NoInputState();
+    void handleEvent(AIFSM& fsm) override;
+    void stateBegin(AIFSM& fsm) override;
+    std::string getStateName() const override { return "No Input"; }
+    void handleManual(AIFSM &fsm) override;
+};
