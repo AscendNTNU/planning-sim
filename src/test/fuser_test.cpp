2cc43f7775a84c01d605553a8fd0590bc0a050d2
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-03-13 22:52:08 +0100

    Add a tentative test for fuser node nearestNeighbor

--- src/test/fuser_test.cpp
+++ src/test/fuser_test.cpp
@@ -3,6 +3,7 @@
 #include "../fuser.h"
 
 World world = World(0);
+#define PI 3.14159
 
 class FuserTest: public ::testing::Test {
 protected:
@@ -11,8 +12,53 @@ protected:
 };
 
 TEST_F (FuserTest, nearestNeighborTest) {
-    Robot robot;
+    // Case 1:
+    // ----------------------
+    // |                    |
+    // |                    |
+    // |                    |
+    // |      ^             |
+    // |      1       2>    |
+    // |                    |
+    // |                    |
+    // ----------------------
+    //
+    // After 2 seconds
+    // ----------------------
+    // |                    |
+    // |      ^             |
+    // |      x             |
+    // |                    |
+    // |                    |
+    // |                    |
+    // |                    |
+    // ----------------------
+    //
+    // Should return x = 1
+
+    Robot robot1 = Robot(1);
+    point_t pos1 = point_zero;
+    pos1.x = 8;
+    pos1.y = 10;
+    robot1.setPositionOrientation(pos1, PI / 2);
+    robots_in_memory[0] = robot1;
+
+    Robot robot2 = Robot(2);
+    point_t pos2 = point_zero;
+    pos2.x = 12;
+    pos2.y = 10;
+    robot2.setPositionOrientation(pos2, 0);
+    robots_in_memory[1] = robot2;
+
+    Robot observed_robot = Robot();
+    point_t posX = point_zero;
+    posX.x = 8;
+    posX.y = 12;
+    observed_robot.setPositionOrientation(posX, PI / 2);
+    //nearestNeighbor(observed_robot); <-- Undefined reference error
+
     std::cout << "works!!" << robots_in_memory.size() << std::endl;
+    std::cout << robots_in_memory[0].getPosition().y << std::endl;
     EXPECT_EQ(1, 1);
 }
 
