dadb283fba4f3bb14c8a5cc23098387e491489c2
adriantre <adrianto@stud.ntnu.no>
2018-04-19 17:58:05 +0200

    Add global variables til config.cpp

--- src/tools/config.cpp
+++ src/tools/config.cpp
@@ -7,18 +7,34 @@ std::set<std::string> Config::missing_param_set_;
 
 //===============  Global config params  =================
 
-//--------------------  Topic nodes  ---------------------
-
 std::string Config::CONTROL_FSM_ACTION_SERVER;
 std::string Config::AI_SIM_OBSERVATION_TOPIC;
-double TOTAL_COMPETITION_TIME;
-double Config::ROS_RATE_PERCEPTION_CONTROL;
-double Config::ROS_RATE_PLANNING;
-
-//------------------  World variables  -------------------
-
-double Config::TOTAL_COMPETITION_TIME;
-
+float Config::TOTAL_COMPETITION_TIME;
+float Config::ROS_RATE_PERCEPTION_CONTROL;
+float Config::ROS_RATE_PLANNING;
+float Config::ROBOT_REVERSE_INTERVAL;
+float Config::ROBOT_TURN_TIME;
+float Config::ROBOT_SPEED;
+float Config::LAND_TIME;
+float Config::MAX_GROUND_TIME;
+float Config::DRONE_SPEED;
+float Config::SIMILARITY_THRESHOLD;
+float Config::MAXDIST_DRONE_TO_POINT;
+float Config::MAXDIST_ROBOT_TO_POINT; 
+float Config::MAXDIST_ACTIONPOINTS;
+float Config::INITIAL_PLANK_REWARD;
+float Config::ACTION_EDGE_BUFFER;
+float Config::RELIABLE_DATA_HEIGHT;
+float Config::POINT_OUTSIDE_OF_PLANK_TOLERANCE;
+float Config::ROBOT_TIME_DRIFT;
+bool Config::PARTIAL_OBSERVABILITY;
+int Config::NUMBER_OF_ROBOTS;
+int Config::NUMBER_OF_OBSTACLES;
+int Config::NUM_PLANK_POINTS;
+int Config::PLANK_POINT_SHRINKAGE;
+int Config::GRID_BOUNDS_X;
+int Config::GRID_BOUNDS_Y;
+int Config::NUM_PLANK_ITERATIONS;
 
 void Config::loadParams() {
     // if(!ros::isInitialized()) {
@@ -28,6 +44,20 @@ void Config::loadParams() {
     ros::NodeHandle n("~");
     auto getDoubleParam = [&](const std::string& name, double& var, double min, double max) {
         double temp;
+        if(!n.getParam(name, temp) || temp < min || temp > max) {
+            std::string warn_msg = "Load param failed: ";
+            warn_msg += name;
+            warn_msg += ", using ";
+            warn_msg += std::to_string(var);
+            missing_param_set_.insert(name);
+            ROS_WARN("%s", warn_msg.c_str());
+        } else {
+            var = temp;
+        }
+    };
+
+    auto getFloatParam = [&](const std::string& name, float& var, float min, float max) {
+        float temp;
         if(!n.getParam(name, temp) || temp < min || temp > max) {
             std::string warn_msg = "Load param failed: ";
             warn_msg += name;
@@ -78,6 +108,30 @@ void Config::loadParams() {
 
     getStringParam("control_fsm_action_server",  CONTROL_FSM_ACTION_SERVER);
     getStringParam("ai_sim_observation_topic", AI_SIM_OBSERVATION_TOPIC);
-    getDoubleParam("total_competition_time", TOTAL_COMPETITION_TIME, 0.0, 610.0);
-    getDoubleParam("ros_rate_perception_control", ROS_RATE_PERCEPTION_CONTROL, 1.0, 400.0);
+    getFloatParam("total_competition_time", TOTAL_COMPETITION_TIME, 0.01, 610.0);
+    getFloatParam("ros_rate_perception_control", ROS_RATE_PERCEPTION_CONTROL, 1.0, 400.0);
+    getFloatParam("ros_rate_planning", ROS_RATE_PLANNING, 1.0, 400.0);
+    getFloatParam("robot_reverse_interval", ROBOT_REVERSE_INTERVAL, 0.01, 40.0);
+    getFloatParam("robot_turn_time", ROBOT_TURN_TIME, 0.01, 5.0);
+    getFloatParam("robot_speed", ROBOT_SPEED, 0.01, 10.0);
+    getFloatParam("land_time", LAND_TIME, 0.01, 10);
+    getFloatParam("max_ground_time", MAX_GROUND_TIME, 0.1, 30.0);
+    getFloatParam("drone_speed", DRONE_SPEED, 0.01, 10.0);
+    getFloatParam("similarity_threshold", SIMILARITY_THRESHOLD, 0.01, 10.0);
+    getFloatParam("maxdist_drone_to_point", MAXDIST_DRONE_TO_POINT, 0.01, 10.0);
+    getFloatParam("maxdist_robot_to_point", MAXDIST_ROBOT_TO_POINT, 0.01, 10.0);
+    getFloatParam("maxdist_actionpoints", MAXDIST_ACTIONPOINTS, 0.01, 30.0);
+    getFloatParam("initial_plank_reward", INITIAL_PLANK_REWARD, -10000000.0, 10000000.0);
+    getFloatParam("action_edge_buffer", ACTION_EDGE_BUFFER, 0.0, 10.0);
+    getFloatParam("reliable_data_height", RELIABLE_DATA_HEIGHT, 0.0, 3.0);
+    getFloatParam("point_outside_of_plank_tolerance", POINT_OUTSIDE_OF_PLANK_TOLERANCE, 0.01, 5.0);
+    getFloatParam("robot_time_drift", ROBOT_TIME_DRIFT, 0.0, 10.0);
+    getBoolParam("partial_observability", PARTIAL_OBSERVABILITY);
+    getIntParam("number_of_robots", NUMBER_OF_ROBOTS, 1, 10);
+    getIntParam("number_of_obstacles", NUMBER_OF_OBSTACLES, 1, 4);
+    getIntParam("num_plank_points", NUM_PLANK_POINTS, 1, 20);
+    getIntParam("plank_point_shrinkage", PLANK_POINT_SHRINKAGE, 0, 5);
+    getIntParam("grid_bounds_x", GRID_BOUNDS_X, 1, 30);
+    getIntParam("grid_bounds_y", GRID_BOUNDS_Y, 1, 30);
+    getIntParam("num_plank_iterations", NUM_PLANK_ITERATIONS, 1, 20);
 }
--- src/tools/config.h
+++ src/tools/config.h
@@ -22,33 +22,32 @@ namespace planning {
         public:
             static std::string CONTROL_FSM_ACTION_SERVER;
             static std::string AI_SIM_OBSERVATION_TOPIC;
-            static double TOTAL_COMPETITION_TIME;
-            
-            static double ROS_RATE_PERCEPTION_CONTROL;
-            static double ROS_RATE_PLANNING;
+            static float TOTAL_COMPETITION_TIME;
+            static float ROS_RATE_PERCEPTION_CONTROL;
+            static float ROS_RATE_PLANNING;
             static float ROBOT_REVERSE_INTERVAL;
-            static double ROBOT_TURN_TIME;
-            static double ROBOT_SPEED;
-            static double LAND_TIME;
+            static float ROBOT_TURN_TIME;
+            static float ROBOT_SPEED;
+            static float LAND_TIME;
             static float MAX_GROUND_TIME;
-            static double DRONE_SPEED;
+            static float DRONE_SPEED;
             static float SIMILARITY_THRESHOLD;
             static float MAXDIST_DRONE_TO_POINT;
             static float MAXDIST_ROBOT_TO_POINT; 
             static float MAXDIST_ACTIONPOINTS;
-            static bool PARTIAL_OBSERVABILITY;
             static float INITIAL_PLANK_REWARD;
+            static float ACTION_EDGE_BUFFER;
+            static float RELIABLE_DATA_HEIGHT;
+            static float POINT_OUTSIDE_OF_PLANK_TOLERANCE;
+            static float ROBOT_TIME_DRIFT;
+            static bool PARTIAL_OBSERVABILITY;
             static int NUMBER_OF_ROBOTS;
             static int NUMBER_OF_OBSTACLES;
             static int NUM_PLANK_POINTS;
             static int PLANK_POINT_SHRINKAGE;
             static int GRID_BOUNDS_X;
             static int GRID_BOUNDS_Y;
-            static float ACTION_EDGE_BUFFER;
-            static float RELIABLE_DATA_HEIGHT;
             static int NUM_PLANK_ITERATIONS;
-            static float POINT_OUTSIDE_OF_PLANK_TOLERANCE;
-            static float ROBOT_TIME_DRIFT;
 
             static void loadParams();    
             ///Returns set of unloaded params
