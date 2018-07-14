38f38aa69f1b345bdfc4f33356345bf1cdceef07
ramunter <rasmusmunter@gmail.com>
2018-07-14 12:38:29 +0200

    filter exception only for sidecameras

--- src/fuser.cpp
+++ src/fuser.cpp
@@ -19,56 +19,6 @@ ros::Time start_time(0.0);
 double simulation_time = 0.0; // This is set by a callback if we are using ai-sim
 point_t drone_position = point_zero;
 
-// Callbacks
-void aiSimCallback(ascend_msgs::AIWorldObservation::ConstPtr obs){ 
-   
-    simulation_time = obs->elapsed_time;  
-   
-    drone_position.x = obs->drone_position.x;  
-    drone_position.y = obs->drone_position.y;  
-    drone_position.z = obs->drone_position.z;  
-   
-    int i = 0; 
-    std::vector<Robot> robots_seen_in_one_message; 
-    for(auto it = obs->ground_robots.begin(); it != obs->ground_robots.end(); it++, i++) { 
-        if(it->visible){   
-            Robot robot;   
-   
-            point_t position;  
-            position.x = it->x;    
-            position.y = it->y;    
-            float q = it->theta;   
-            float time = simulation_time; 
-            bool visible = true;   
-            robot.setSideCamera(true);
-            robot.update(i, position, q , time, visible);
-            
-            if(robot.isInArena()){  
-                robots_seen_in_one_message.push_back(robot);
-            }
-        }  
-    }  
-    observed_robots.push_back(robots_seen_in_one_message); 
-   
-    i = 0; 
-    std::vector<Robot> obstacle_robots_seen_in_one_message;    
-    for(auto it = obs->obstacle_robots.begin(); it != obs->obstacle_robots.end(); it++, i++){  
-        if(it->visible){   
-            Robot robot;   
-            point_t position;  
-            position.x = it->x;    
-            position.y = it->y;    
-            float q = it->theta;   
-            float time = simulation_time; 
-            bool visible = true;   
-   
-            robot.update(i, position, q , time, visible);  
-            obstacle_robots_seen_in_one_message.push_back(robot);  
-        }  
-    }  
-    observed_obstacle_robots.push_back(obstacle_robots_seen_in_one_message);   
-}
-
 void groundRobotCallback(ascend_msgs::DetectedRobotsGlobalPositions::ConstPtr msg){
     
     if(drone_position.z < 0.4){
@@ -94,9 +44,9 @@ void groundRobotCallback(ascend_msgs::DetectedRobotsGlobalPositions::ConstPtr ms
         robots_seen_in_one_message.push_back(robot);
         
         if(msg->robot_color.at(i)!=3){
-                robots_seen_in_one_message.push_back(robot);
+            robots_seen_in_one_message.push_back(robot);
         }else{
-                obstacle_robots_seen_in_one_message.push_back(robot);
+            obstacle_robots_seen_in_one_message.push_back(robot);
         }
 
        if(msg->camera_type.at(i) == 0){
@@ -139,18 +89,6 @@ void initializeFuser(){
         double orientation = t;
         robots_in_memory.at(i).update(i,point,orientation, 0, false);
     }
-
-    // for (unsigned int i = 0; i < obstacle_robots_in_memory.size(); i++){
-    //     double t =3.14*2.0 * i / (double)obstacle_robots_in_memory.size();
-
-    //     // The obstacles are also spawned in a circle,
-    //     // but at an initial radius of 5 meters.
-    //     point_t point;
-    //     point.x = 10.0f + 5 * cosf(t);
-    //     point.y = 10.0f + 5 * sinf(t);
-    //     double orientation = t;
-    //     obstacle_robots_in_memory.at(i).update(i, point, orientation, 0, false);
-    // }
 }
 
 std::set<int> updateRobots(std::vector<Robot> robots_in_single_message, std::vector<KalmanRobot> &memory, double current_time){
@@ -234,10 +172,8 @@ ascend_msgs::AIWorldObservation createObservation(double current_time){
 
         if(robots_in_memory.at(i).getVisible()){
             bool is_reliable = isModelStillReliable(robots_in_memory.at(i), drone_position, current_time);
-
             robots_in_memory.at(i).setVisible(is_reliable);
         }
-        
         robot.visible = robots_in_memory.at(i).getVisible();
         observation.ground_robots.at(i) = robot;
     }
@@ -252,13 +188,6 @@ ascend_msgs::AIWorldObservation createObservation(double current_time){
     observation.elapsed_time = current_time;
 
     return observation;
-}
-
-double calcCurrentTime(ros::Time seconds){
-    if(simulation_time == 0.0){
-        return (seconds-start_time).toSec(); 
-    }
-    return simulation_time;
 }
 
 int main(int argc, char **argv){
@@ -291,26 +220,21 @@ int main(int argc, char **argv){
         double current_time = calcCurrentTime(ros::Time::now());
 
         if(USE_FUSER){
-            std::cout << "TICK STARTED" << std::endl;
             fuser_tick(robots_in_memory, current_time);
-            std::cout << "TICK DONE" << std::endl;
         }
-        else{
 
+        else{
             int i = 0;
             if(observed_robots.size() != 0){
                 std::vector<Robot> last_observation = *observed_robots.begin();
                 
-                for(auto it = last_observation.begin(); it != last_observation.end(); it++, i++){
-                    
+                for(auto it = last_observation.begin(); it != last_observation.end(); it++, i++){       
                     if(i >= NUMBER_OF_ROBOTS){
                         break;
                     }
-
                     robots_in_memory.at(i).update(*it);
                 }
             }
-
         }
 
         observed_robots.clear();
@@ -323,4 +247,11 @@ int main(int argc, char **argv){
 
     return 0;
 
+}
+
+double calcCurrentTime(ros::Time seconds){
+    if(simulation_time == 0.0){
+        return (seconds-start_time).toSec(); 
+    }
+    return simulation_time;
 }
